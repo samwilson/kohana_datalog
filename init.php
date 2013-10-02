e64771ce465454c1f22505dc8c300badbced9e83
Sam Wilson <sam@samwilson.id.au>
2013-10-02 10:53:57 +0800

    Make route accept non-identifying URLs as well, even though these will return nothing (they are still part of DataLog).

--- init.php
+++ init.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
-Route::set('datalog', 'datalog/<table_name>/<row_pk>')->defaults(array(
+Route::set('datalog', 'datalog/<table_name>(/<row_pk>)')->defaults(array(
 	'controller' => 'DataLog',
 	'action'     => 'index',
 ));
