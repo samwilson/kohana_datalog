107f5972afe7d92b081fe8603f80b208f4ca4f6c
Kenny <JustLikeIcarus@gmail.com>
2013-10-29 04:56:36 -0400

    Updating Core to auto detect one-to-one relationships if $belongs_to is passed in.
    Changed from _related to _belongs_to since we only care about one-to-one relationships and ORM builds the _belongs_to on init.
    
    Made $belongs_to an optional parameter for logging of tables that have no foreign keys.
    
    Created a function for searching the $belongs_to array and pull the model name out of it if the column is a foreign key.
    
    Added in the Kohana coding standards... I think i got it right.

--- classes/DataLog/Core.php
+++ classes/DataLog/Core.php
@@ -1,47 +1,87 @@
+
 <?php defined('SYSPATH') OR die('No direct script access.');
 
 class DataLog_Core {
 
-	/** @var string The name of the table being logged. */
-	protected $table_name;
+	/**
+	* @var array The name of the table being logged.
+	*/
+	protected $_table_name;
 
-	/** @var array */
-	protected $old_values;
+	/**
+	* @var array Original values of model being logged
+	*/
+	protected $_old_values;
 
-	/** @var string The suffix used to designate foreign key fields. */
-	protected $foreign_key_suffix = '_id';
-
+	/**
+	 * Constructs a new instance of DataLog
+	 *
+	 * @param string $table_name Table name for model being logged
+	 * @param array  $old_values $_original_values property for model being logged
+	 */
 	public function __construct($table_name, $old_values)
 	{
-		$this->table_name = $table_name;
-		$this->old_values = $old_values;
+		$this->_table_name = $table_name;
+		$this->_old_values = $old_values;
 	}
 
-	public function save($row_pk, $object, $related)
+	/**
+	* Check the field to see if it is a foreign key to another model
+	* and return the foreign models name if it is
+	*
+	* @param  string $field  Field to search for
+	* @param  array  $belongs_to = NULL $_belongs_to property for model being saved
+	* @return string Name of foreign model
+	*/
+	protected function _foreign_model_search($field, array $belongs_to = NULL)
+	{
+		if (is_array($belongs_to))
+		{
+			foreach ($belongs_to as $key => $value)
+			{
+				$current_key = $key;
+				if ( ($field === $value) OR (is_array($value) AND (bool) $this->_foreign_model_search($field,$value)))
+				{
+					return $belongs_to[$current_key]['model'];
+				}
+			}
+		}
+		return FALSE;
+	}
+
+	/**
+	* Check data being saved and create log entries for modified values
+	*	
+	* @param	int    $row_pk  Primary Key of model being saved
+	* @param	array  $belongs_to = NULL $_belongs_to property of model being saved
+	* @return	string  Name of foreign model
+	*/
+	public function save($row_pk, $object, array $belongs_to = NULL)
 	{
 		foreach ($object as $field => $new_datum)
 		{
-			// Handle foreign keys
-			$prefix = substr($field, 0, -strlen($this->foreign_key_suffix));
+			// Check if current field is a foreign key
+			$foreign_model = $this->_foreign_model_search($field, $belongs_to);
 			
-			if (isset($related[$prefix]))
+			// If foreign model exists set value using candidate_key value
+			if ( (bool) $foreign_model)
 			{
-				$old_datum = isset($this->old_values[$field]) ? $this->old_values[$field] : null;
-				$foreign_model = $related[$prefix]['model'];
+				$old_datum = Arr::get($this->_old_values, $field, NULL);
 				$old_value = ORM::factory($foreign_model, $old_datum)->candidate_key();
 				$new_value = ORM::factory($foreign_model, $new_datum)->candidate_key();
 			}
 			// Handle normal direct values
 			else
 			{
-				$old_value = Arr::get($this->old_values, $field, NULL);
+				$old_value = Arr::get($this->_old_values, $field, NULL);
 				$new_value = $new_datum;
 			}
+			
 			// Save the log entry
 			if ($new_value != $old_value)
 			{
 				$log_entry = ORM::factory('DataLog');
-				$log_entry->table_name = $this->table_name;
+				$log_entry->table_name = $this->_table_name;
 				$log_entry->column_name = $field;
 				$log_entry->row_pk = $row_pk;
 				$log_entry->old_value = $old_value;
@@ -50,5 +90,4 @@ class DataLog_Core {
 			}
 		}
 	}
-
 }
